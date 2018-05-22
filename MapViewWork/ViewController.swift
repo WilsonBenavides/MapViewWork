5473085d50ca9d5b97c4e75a370907ec99dc6399
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-22 16:44:01 -0700

    display map programmatically in popayan coords

--- MapViewWork/ViewController.swift
+++ MapViewWork/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var window: UIWindow?
+    var mapView: MKMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.view.backgroundColor = .red
+        
+        self.mapView = MKMapView(frame: CGRect(x: 0, y: 20, width: (self.window?.frame.width)!, height: 300))
+        self.view.addSubview(self.mapView!)        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
