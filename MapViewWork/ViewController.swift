96153a0b657c7cfe94460e6b971e562b07de036f
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-22 20:20:18 -0700

    display map with custom coordinates and span

--- MapViewWork/ViewController.swift
+++ MapViewWork/ViewController.swift
@@ -9,19 +9,33 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     var window: UIWindow?
-    var mapView: MKMapView?
+    var mapView = MKMapView()
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.view.backgroundColor = .red
         
-        self.mapView = MKMapView(frame: CGRect(x: 0, y: 20, width: (self.window?.frame.width)!, height: 300))
-        self.view.addSubview(self.mapView!)        
+        
+        let loc = CLLocationCoordinate2DMake(2.4430307,-76.7184948)
+        let span = MKCoordinateSpanMake(0.08, 0.08)
+        let reg = MKCoordinateRegionMake(loc, span)
+        self.mapView.setRegion(reg, animated: true)
+        
+        mapView.frame = CGRect(x: 0, y: 20, width: (self.window?.frame.width)!, height: (self.window?.frame.height)! / 2)
+        
+        //self.mapView = MKMapView(frame: CGRect(x: 0, y: 20, width: (self.window?.frame.width)!, height: 300))
+        self.view.addSubview(self.mapView)
     }
 }
 
---
+++ Podfilees
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'MapViewWork' do
+pod 'GoogleMaps'
+pod 'GooglePlaces'
+end
