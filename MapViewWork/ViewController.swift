56562867c63b56359627c6268a968b91d67bb4b6
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-05-23 19:28:31 -0700

    rectangle with four location coordinates

--- MapViewWork/ViewController.swift
+++ MapViewWork/ViewController.swift
@@ -9,27 +9,10 @@
 import UIKit
 import GoogleMaps
 
-class VacationDestination: NSObject {
-    
-    let name: String
-    let location:  CLLocationCoordinate2D
-    let zoom: Float
-    
-    init(name: String, location: CLLocationCoordinate2D, zoom: Float) {
-        self.name = name
-        self.location = location
-        self.zoom = zoom
-    }
-}
-
 class ViewController: UIViewController {
     
     var window: UIWindow?
     var mapView: GMSMapView?
-    
-    var currentDestination: VacationDestination?
-    
-    let destinations = [VacationDestination(name: "Estadio", location: CLLocationCoordinate2DMake(2.454723, -76.591655), zoom: 15), VacationDestination(name: "Cementerio", location: CLLocationCoordinate2DMake(2.448356, -76.620785), zoom: 16)]
     
     let buttonTest: UIButton = {
         let yPos = UIScreen.main.bounds.height - 50
@@ -45,16 +28,6 @@ class ViewController: UIViewController {
     
     @objc func handleBtn() {
         print(123)
-        if currentDestination == nil {
-            currentDestination = destinations.first
-            
-        }else {
-            
-            if let index = destinations.index(of: currentDestination!) {
-                currentDestination = destinations[index + 1]
-            }
-        }
-        setMapCamera()
     }
     
     override func viewDidLoad() {
@@ -62,36 +35,25 @@ class ViewController: UIViewController {
         GMSServices.provideAPIKey("AIzaSyD0L_riQOEn9ngRmYiE8ko6HZ24AbzSshk")
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .white
         
-        let camera = GMSCameraPosition.camera(withLatitude: 2.464266, longitude: -76.580880, zoom: 12)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.40, longitude: -122.1, zoom: 11)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         
+        let path = GMSMutablePath()
+        path.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0))
+        path.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.0))
+        path.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.2))
+        path.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.2))
+        path.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0))
+        
+        let rectangle = GMSPolyline(path: path)
+        rectangle.map = mapView
+
         mapView?.frame = CGRect(x: 0, y: 20, width: (self.window?.frame.width)!, height: (self.window?.frame.height)! * 3 / 4)
-        
-        let currentLocation = CLLocationCoordinate2DMake(2.464266, -76.580880)
-        let markerFiet = GMSMarker(position: currentLocation)
-        markerFiet.title = "Club Campestre"
-        markerFiet.map = mapView
-        
-        
         
         self.view.addSubview(mapView!)
         self.view.addSubview(buttonTest)
-    }
-    
-    private func setMapCamera() {
-        
-        CATransaction.begin()
-        CATransaction.setValue(2, forKey: kCATransactionAnimationDuration)
-        
-        mapView?.animate(to: GMSCameraPosition.camera(withTarget: currentDestination!.location, zoom: currentDestination!.zoom))
-        
-        CATransaction.commit()
-        
-        let marker = GMSMarker(position: currentDestination!.location)
-        marker.title = currentDestination?.name
-        marker.map = mapView
     }
 }
 
